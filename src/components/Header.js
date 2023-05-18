60abc52be06d77fb6b9afba679d3040b952ac2c6
Fatemeh Ragh <fateme.ragh@gmail.com>
2023-05-18 19:16:44 +0330

    header styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,18 +4,25 @@ import {ThemeContext} from "@/contexts/ThemeContext";
 //styles
 import styles from '../styles/Header.module.css';
 
+
+
 //----------------------------------------------------------------
 
 const Header = () => {
-    const {toggleTheme} = useContext(ThemeContext);
+    const {theme, toggleTheme} = useContext(ThemeContext);
+
+    const headerStyle = {
+        backgroundColor: theme === 'dark' ? 'var(--dark-blue)' : 'var(--white)',
+        color: theme === 'dark' ? 'var(--white)' : 'var(--very-dark-blue-light-text)',
+    };
 
     return (
-        <header className={`${styles.header}`}>
-            <span style={{fontWeight: '800'}}>
-                Where in the world?
-            </span>
-            <button onClick={toggleTheme}>
-                jo;;j
+        <header className={`${styles.header} shadow`} style={headerStyle}>
+      <span style={{fontWeight: '800'}}>
+        Where in the world?
+      </span>
+            <button onClick={toggleTheme} style={headerStyle}>
+                {theme === 'dark' ? 'Dark Mode' : 'Light Mode'}
             </button>
         </header>
     );
